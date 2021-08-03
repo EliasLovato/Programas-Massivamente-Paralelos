808235ac499768f3f76b0fb911652d7d68706bb0
Elias Lovato <eliasgcl@yahoo.com.br>
2021-08-02 21:18:05 -0300

    slurm e programa f90

--- Lista 03/prog-trans_modified.f90
+++ Lista 03/prog-trans_modified.f90
@@ -1,18 +1,17 @@
-        program transpose
+program transpose
         
-        IMPLICIT NONE
+	IMPLICIT NONE
         
 	INCLUDE 'f90papi.h'
 
         double precision, allocatable :: a(:,:)
         double precision, allocatable :: b(:,:)
-        double precision t1, t2, fn, rate, mysecond
+        double precision :: t1, t2, fn, rate, mysecond
         
-        INTEGER, DIMENSION(5) :: events = &
-	&(/PAPI_LD_INS, PAPI_SR_INS, PAPI_L1_TCM, PAPI_L2_TCM, PAPI_L3_TCM/)
+        INTEGER, DIMENSION(5) :: events = (/PAPI_LD_INS, PAPI_SR_INS, PAPI_L1_TCM, PAPI_L2_TCM, PAPI_L3_TCM/)
         INTEGER*8, DIMENSION(5) :: avalues
         INTEGER*8, DIMENSION(5) :: bvalues
-	INTEGER :: check
+	INTEGER :: I, J, N, check
 
         call pegavalor(N)
         fn = n
@@ -24,19 +23,17 @@
         
         t1 = mysecond()
 
-	CALL PAPIF_start_counters(events, 5, check)
-	
+	call PAPIF_start_counters(events, 5, check)
 	if (check .ne. PAPI_OK) stop
-	
-	        
+
+
 	do J=1,N
-	  do I=1,N
-	    B(I,J)=A(J,I)
-	  enddo
+		do I=1,N
+			B(I,J)=A(J,I)
+		enddo
 	enddo
 	
-	CALL PAPIF_read_counters(avalues, 5, check)
-	
+	call PAPIF_read_counters(avalues, 5, check)
 	if (check .ne. PAPI_OK) stop
 	        
 	t2 = mysecond()
@@ -45,7 +42,8 @@
 
         call confere(A,B,N)
         print *,"N=",N," T=",t2-t1," Rate=",rate,"MB/s"
-        print *, "PAPI_LD_INS=", avalues(1)," PAPI_SR_INS=", avalues(2)," PAPI_L1_TCM=", avalues(3)," PAPI_L2_TCM=", avalues(4)," PAPI_L3_TCM=", avalues(5)
+        
+	print *,"PAPI_LD_INS=",avalues(1)," PAPI_SR_INS=",avalues(2)," PAPI_L1_TCM=",avalues(3)," PAPI_L2_TCM=",avalues(4)," PAPI_L3_TCM=",avalues(5)
 
         stop
-        end
+end
